8d3fedb0185223b6c615b3a441232bc83bb6741b
bryanparmelee <bryan.parmelee@gmail.com>
2022-08-18 23:12:22 -0400

    Centralize firebase auth with context

--- src/components/sign-in-form/sign-in-form.compoonent.jsx
+++ src/components/sign-in-form/sign-in-form.compoonent.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+
+
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
@@ -22,24 +24,23 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
-    
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
 
   
     const signInWithGoogle = async () => {
-            const {user} = await signInWithGooglePopup();
-            await createUserDocumentFromAuth(user);
-        };   
+        await signInWithGooglePopup();
+    };   
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         
         try {  
-            const response = await signInAuthUserWithEmailAndPassword(email, password);     
-            console.log(response);   
+            const { user } = await signInAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );     
             resetFormFields();
 
         } catch(error) {
