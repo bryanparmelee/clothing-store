0eecbeef748ece709b424010be1f4fdd3c350b29
bryanparmelee <bryan.parmelee@gmail.com>
2022-08-23 14:48:24 -0400

    Add order total and item removal to checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,10 +2,12 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import './checkout.styles.scss';
 
 const Checkout = () => {
-    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
     return (
         <div className='checkout-container'>
             <div className='checkout-header'>
@@ -26,25 +28,13 @@ const Checkout = () => {
                 </div>
             </div>           
                 {
-                    cartItems.map((cartItem) => {
-                        const { id, name, quantity } = cartItem;
-                        return (
-                            <div key={id}>
-                                <h2>{name}</h2>
-                                <span>{quantity}</span>
-                                <br />
-                                <span onClick={() => removeItemFromCart(cartItem)} >decrement</span>
-                                <br />
-                                <span onClick={() => {addItemToCart(cartItem)} }>increment</span>
-                                
-                            </div>
-                        )
-                    })
-                }
-            <span className='total'>Total</span>
+                    cartItems.map((cartItem) => (                                               
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem} />                        
+                    ))}
+            <span className='total'>Total: ${cartTotal}</span>
         </div>
 
-    )
-}
+    );
+};
 
 export default Checkout;
