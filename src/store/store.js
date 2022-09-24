f81806beccf2c39e30d7928eed841a3bf4b26194
bryanparmelee <bryan.parmelee@gmail.com>
2022-09-24 18:02:03 -0400

    Convert category thunks to sagas

--- src/store/store.js
+++ src/store/store.js
@@ -3,21 +3,25 @@ import { legacy_createStore as createStore} from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'; 
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from '@redux-saga/core';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart'],
 }
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
     process.env.NODE_ENV === 'development' && logger, 
-    thunk,
+    sagaMiddleware
 ].filter(Boolean);
 
 const composeEnhancer = 
@@ -33,5 +37,7 @@ export const store = createStore(
         undefined, 
         composedEnhancers
     );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
