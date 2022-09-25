c5182f23a2850a2f424b870525f14f55911e31e1
bryanparmelee <bryan.parmelee@gmail.com>
2022-09-25 11:44:51 -0400

    Convert signup thunk to sagas

--- src/components/sign-up-form/sign-up-form.compoonent.jsx
+++ src/components/sign-up-form/sign-up-form.compoonent.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -10,6 +11,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import { SignUpContainer } from './sign-up-form.styles';
+import { signUpStart } from '../../store/user/user.action';
 
 const defaultFormFields = {
     displayName: '',
@@ -22,6 +24,7 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+    const dispatch = useDispatch();
 
  
 
@@ -38,12 +41,7 @@ const SignUpForm = () => {
         }
 
         try {
-            const { user } = await createAuthUserWithEmailAndPassword(
-                email, 
-                password
-            );
-   
-            await createUserDocumentFromAuth(user, { displayName });            
+            dispatch(signUpStart(email, password, displayName));        
             resetFormFields();
 
         } catch(error) {
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -3,7 +3,7 @@ import { USER_ACTION_TYPES } from "./user.types";
 const INITIAL_STATE = {
     currentUser: null,
     isLoading: false,
-    erorr: null,
+    error: null,
 }
 
 export const userReducer = (state = INITIAL_STATE, action) => { 
@@ -13,7 +13,7 @@ export const userReducer = (state = INITIAL_STATE, action) => {
         case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
             return { ...state, currentUser: payload };
         case USER_ACTION_TYPES.SIGN_IN_FAILED:
-            return { ...state, erorr: payload };
+            return { ...state, error: payload };
         default:
             return state;
             
