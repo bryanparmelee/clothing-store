d1f839d115d799bb5ddbc1455b5ce4d83271f281
bryanparmelee <bryan.parmelee@gmail.com>
2022-09-25 18:36:07 -0400

    Add stripe payment integration

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import './index.css';
 import App from './App';
 
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe';
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -16,8 +19,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>       
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
             <App />
+          </Elements>       
         </BrowserRouter>
       </PersistGate>
     </Provider>
