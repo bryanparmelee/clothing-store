dd59060858ccb9824b44c9d12831fd7334c0dc79
bryanparmelee <bryan.parmelee@gmail.com>
2022-09-25 12:04:50 -0400

    Migrate signout to sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,30 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from "../../store/user/user.selection";
+import { signOutSart } from '../../store/user/user.action';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
-import { isCartOpen, selectIsCartOpen } from '../../store/cart/cart.selector';
-import { selectCurrentUser } from "../../store/user/user.selection";
 
-import { signOutUser } from "../../utils/firebase/firebase.utils";
-
-import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
+import { 
+  NavigationContainer, 
+  NavLinks, 
+  NavLink, 
+  LogoContainer 
+} from './navigation.styles';
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutSart());
 
     return (
       <Fragment>
